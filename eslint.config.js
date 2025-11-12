4f9b51d3c906a63377ff299becf1e12729f8c311
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2025-11-12 23:10:47 +0800

    chore: update lavy

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,11 @@ import { defineConfig } from 'eslint/config'
 import tsConfig from 'eslint-config-lavy/ts'
 
 export default defineConfig([
-  { ignores: ['example/**'] },
   ...tsConfig,
   {
     files: ['**/*.{ts}'],
     rules: {
-      // 你可以在这里添加项目特定的规则
+      // You can add project-specific rules here.
     }
   }
 ])
